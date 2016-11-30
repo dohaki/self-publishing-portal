533182129550026831108637bdf151a54766c3ff
Dong-Ha Kim <dong.kim@junior-comtec.de>
2016-11-30 17:25:56 +0100

    campaign background tab

---
+++ client/templates/components/campaignCreateBackground.js
@@ -0,0 +1,21 @@
+import {Template} from 'meteor/templating';
+
+import './campaignCreateBackground.html';
+
+Template.components_campaignCreateBackground.onRendered(function () {
+    $(function() {
+        $('div#froala-editor').froalaEditor({
+        })
+    });
+});
+
+Template.components_campaignCreateBackground.events({
+    // geht ein Tab weiter nach rechts
+    'click .js-next'() {
+        $('ul.tabs').tabs('select_tab', 'author');
+    },
+    // geht ein Tab weiter nach links
+    'click .js-back'() {
+        $('ul.tabs').tabs('select_tab', 'rewards');
+    }
+});
