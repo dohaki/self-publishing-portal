9ecd1195b973117079c3d16d98284be68a77b8af
Dong-Ha Kim <dong.kim@junior-comtec.de>
2016-12-01 16:13:26 +0100

    sidenav mockup

--- client/templates/layout/sidebar.js
+++ client/templates/layout/sidebar.js
@@ -1,11 +1,14 @@
-import { Template } from 'meteor/templating';
+import {Template} from 'meteor/templating';
 
 import './sidebar.html';
 
+Template.layout_sidebar.onRendered(function () {
+});
+
 Template.layout_sidebar.helpers({
-
+    account () {
+        return EthAccounts.find().fetch()[0];
+    }
 });
 
-Template.layout_sidebar.events({
-
-});
+Template.layout_sidebar.events({});
--- client/templates/views/login.js
+++ client/templates/views/login.js
@@ -6,11 +6,14 @@ import './login.html';
 FlowRouter.route('/login', {
     name: 'login',
     action: function () {
-        $(document).ready(function() {
-            $('select').material_select();
-        });
         BlazeLayout.render('views_login');
     }
+});
+
+Template.views_login.onRendered(function () {
+    $(document).ready(function() {
+        $('select').material_select();
+    });
 });
 
 Template.views_login.helpers({
@@ -21,13 +24,13 @@ Template.views_login.events({
     'click .js-create'(event) {
         const username = $('#username').val();
         const role = $('#role').val();
-
         UserRegisterContract.join(username, role, function (error, result) {
             if (error) {
                 console.error('ERROR in login.js');
             } else {
                 console.log('SUCCESS');
                 console.log(result);
+                // TODO username und role in lokaler DB oder Session speichern
             }
         });
     }
