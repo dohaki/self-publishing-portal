d7578d4d35586b2a3b3776db5842e2bc6264df5b
Dong-Ha Kim <dong.kim@junior-comtec.de>
2016-12-01 09:21:44 +0100

    header.js init von sidenav angepasst

--- client/templates/layout/header.js
+++ client/templates/layout/header.js
@@ -1,14 +1,13 @@
-import { Template } from 'meteor/templating';
+import {Template} from 'meteor/templating';
 
 import './header.html';
 
-Template.layout_header.helpers({
-
-});
-
-Template.layout_header.events({
-    'click .button-collapse'(event) {
-        // Initialize collapse button
+Template.layout_header.onRendered(function () {
+    $(document).ready(function () {
         $('.button-collapse').sideNav();
-    }
+    });
 });
+
+Template.layout_header.helpers({});
+
+Template.layout_header.events({});
