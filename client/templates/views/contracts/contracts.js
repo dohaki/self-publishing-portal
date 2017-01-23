d7edf5f7e5565664f360a17fdb48e63f542b4262
Dong-Ha Kim <dong.kim@junior-comtec.de>
2017-01-23 12:22:54 +0100

    change button aus contract overview entfernt

--- client/templates/views/contracts/contracts.js
+++ client/templates/views/contracts/contracts.js
@@ -14,18 +14,6 @@ Template.views_contracts.onRendered(() => {
 Template.views_contracts.events({
     'click .js-create-contract' () {
         FlowRouter.go('/contracts/create');
-    },
-    'click .js-change-contract' () {
-        const contractAddress = '0x19ec5ddb164be1a96e6f679aeff7a8949b2e7b8a';
-        const name = 'Veränderter Vertrag'; // max 50 Zeichen
-        const description = 'Veränderter Beschreibungstext'; // max 150 Zeichen
-        const varReward = web3.toWei(2, 'ether');
-        const fixReward = web3.toWei(3, 'ether');
-        const valueTypeId = 1;
-        const contractTypeIds = [1,2];
-        changeContract(contractAddress, name, description, fixReward, varReward, valueTypeId, contractTypeIds, () => {
-            console.log('CHANGE-CONTRACT CALLBACK');
-        });
     }
 });
 
